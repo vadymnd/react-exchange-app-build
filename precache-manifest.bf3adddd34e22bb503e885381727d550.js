fe4eb97c824b2992c9a4268cd1661354e2235ddb
Vadym <51242804+vadymnd@users.noreply.github.com>
2019-11-18 10:52:59 +0200

    v 1.0

---
+++ precache-manifest.bf3adddd34e22bb503e885381727d550.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2deee75823c78e50f4de8208b9185149",
+    "url": "/index.html"
+  },
+  {
+    "revision": "03a9de210d56d05cb3ae",
+    "url": "/static/css/main.ef112f6d.chunk.css"
+  },
+  {
+    "revision": "ae4f75487284a0dd8d1a",
+    "url": "/static/js/2.bb86e246.chunk.js"
+  },
+  {
+    "revision": "03a9de210d56d05cb3ae",
+    "url": "/static/js/main.cb2bb14c.chunk.js"
+  },
+  {
+    "revision": "5ab36d03efb883b4ed9f",
+    "url": "/static/js/runtime-main.b61a03b4.js"
+  }
+]);
